d42abed385df81db4efff42d4ac242515000b92c
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-13 04:07:51 -0500

    feat:cria rota post UserController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -8,3 +9,4 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 // rota pública
+Route::post('users', [UserController::class, 'store']);
