e1c6459455b56c5950779a793ac7b2754f8e7e54
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-14 01:42:08 -0500

    feat:definicao da tabela
    llave estrangeira user_id

--- database/migrations/2023_12_14_062435_create_exercises_table.php
+++ database/migrations/2023_12_14_062435_create_exercises_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('exercises', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('description', 255)->nullable();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
