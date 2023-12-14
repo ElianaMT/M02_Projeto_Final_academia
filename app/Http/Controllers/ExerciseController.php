44728719806613222b8c4377a378b79099dea89f
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-14 02:05:12 -0500

    feat: configura controller

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Exercise;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ExerciseController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+
+        try {
+            $data = $request->all();
+
+            $request->validate([
+                'description' => 'string|required|max:255'
+            ]);
+
+            $exercise = Exercise::create($data);
+
+            $user = User::find($exercise->user_id);
+            
+            
+            return $exercise;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
 }
