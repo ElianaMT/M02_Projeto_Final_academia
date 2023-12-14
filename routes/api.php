039df1aeca60eb76c96b99ff8665f2b12c2778f4
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-14 01:48:54 -0500

    feat:cria rota post-exercises privada

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
+Route::post('exercises', [ExerciseController::class, 'store']);
+
+
 });
 
 // rota pública
