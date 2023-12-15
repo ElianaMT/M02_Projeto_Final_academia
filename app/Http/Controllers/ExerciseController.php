2100059ff8bd3b7c0b620372fa5b191257d5b102
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-15 01:12:08 -0500

    refactor: implementa user_ para la delecao do exercicios
    Incluye status code: 204,403,404

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -15,7 +15,7 @@ class ExerciseController extends Controller
     public function index(Request $request)
     {
         try {
-            $user = Auth::user();            
+            $user = Auth::user();
 
             $exercises = Exercise::where('user_id', $user->id)
                 ->orderBy('description')
@@ -55,17 +55,34 @@ class ExerciseController extends Controller
 
             return $exercise;
         } catch (Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_CONFLICT);//status code 409
+            return $this->error($exception->getMessage(), Response::HTTP_CONFLICT); //status code 409
         }
     }
 
-    public function destroy($id){
-        $exercise = Exercise::find($id);
+    public function destroy($id)
+    {
+        try {
+            // Obtenho usuario autenticado
+            $user = Auth::user();
 
-        if(!$exercise) return $this->error('Dado não encontrado', Response::HTTP_NOT_FOUND);
+            // Encontra o exercicio por id
+            $exercise = Exercise::find($id);
 
-        $exercise->delete();
+            // Verifica si exercicio existe
+            if (!$exercise) {
+                return $this->error('Dado não encontrado', Response::HTTP_NOT_FOUND);
+            }
 
-        return $this->response('',Response::HTTP_NO_CONTENT);
+            // Verifica si usuario autenticado e igual a user_id
+            if ($user->id !== $exercise->user_id) {
+                return $this->error('Nao tem permisos para eliminar este exercicio', Response::HTTP_FORBIDDEN);
+            }
+
+            $exercise->delete();
+
+            return $this->response('Exercício deletado', Response::HTTP_NO_CONTENT);
+        } catch (Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
 }
