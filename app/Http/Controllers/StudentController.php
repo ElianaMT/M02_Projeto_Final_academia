a83e00263b2650a523bce05b7e1934f344086094
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-16 00:05:37 -0500

    feat: cria metodo update
    Adiciona ruler para dados unique

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -53,7 +53,7 @@ class StudentController extends Controller
             // Cria novo estudante y asigna al user_id
             $student = new Student([
                 'user_id' => $user->id,
-                                
+
                 'id' => $request->input('id'),
                 'name' => $request->input('name'),
                 'email' => $request->input('email'),
@@ -67,7 +67,7 @@ class StudentController extends Controller
                 'city' => $request->input('city'),
                 'complement' => $request->input('complement'),
                 'number' => $request->input('number'),
-                
+
 
             ]);
 
@@ -85,10 +85,10 @@ class StudentController extends Controller
             // Obtenho usuario autenticado
             $user = Auth::user();
 
-            // Encontra o exercicio por id
+            // Encontra o estudante por id
             $student = Student::find($id);
 
-            // Verifica si exercicio existe
+            // Verifica si estudante existe
             if (!$student) {
                 return $this->error('Dado não encontrado', Response::HTTP_NOT_FOUND);
             }
@@ -101,6 +101,57 @@ class StudentController extends Controller
             $student->delete();
 
             return $this->response('Estudante deletado', Response::HTTP_NO_CONTENT);
+        } catch (Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            // Obtenho usuario autenticado
+            $user = Auth::user();
+
+            //cria variavel $data
+            $data = $request->all();
+
+            // Valida os dados do request-corpo
+            $request->validate([
+                'name' => 'string|nullable|max:255',
+                'email' => 'string|nullable|max:255|unique:students,email,' . $id,
+                'date_birth' => 'string|nullable|date_format:Y-m-d',
+                'cpf' => 'string|nullable|max:255|unique:students,cpf,' . $id,
+                'contact' => 'string|required|max:20|unique:students,contact,' . $id,
+                'cep' => 'string|nullable',
+                'street' => 'string|nullable',
+                'state' => 'string|nullable',
+                'neighborhood' => 'string|nullable',
+                'city' => 'string|nullable',
+                'complement' => 'string|nullable',
+                'number' => 'string|nullable',
+            ], [
+                'email.unique' => 'O e-mail já está sendo usado',
+                'cpf.unique' => 'O CPF já está sendo usado.',
+                'contact.unique' => 'O contato já está sendo usado',
+            ]);
+
+            // Encontra o estudante por id
+            $student = Student::find($id);
+
+            // Verifica si estudante existe
+            if (!$student) {
+                return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
+            }
+
+            // Verifica si el usuario autenticado es igual al user_id del estudiante
+            if ($user->id !== $student->user_id) {
+                return $this->error('Não tem permissão para atualizar este estudante', Response::HTTP_FORBIDDEN);
+            }
+
+            // Actualiza os dados do estudante
+            $student->update($data);
+
+            return $student;
         } catch (Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
