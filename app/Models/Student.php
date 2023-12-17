e9863f3370b84c67ac322b6a4b3ccdc0f9fce76b
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-17 18:50:40 -0500

    refactor:hidden user_id

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -24,5 +24,5 @@ class Student extends Model
         'user_id'         
                 
     ];
-    protected $hidden = ['created_at','updated_at'];
+    protected $hidden = ['created_at','updated_at','user_id'];
 }
