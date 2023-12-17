59768bafd2df944aba8cf237ac4e03b7c7827152
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-17 18:52:25 -0500

    feat:hidden delete_at

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -24,5 +24,5 @@ class Student extends Model
         'user_id'         
                 
     ];
-    protected $hidden = ['created_at','updated_at','user_id'];
+    protected $hidden = ['created_at','updated_at','user_id','deleted_at'];
 }
