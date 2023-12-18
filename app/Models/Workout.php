eb9a569e93b47bd47d99447a7f24fc1996b02be8
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-17 23:33:18 -0500

    feat:cria fillable e hidden en model Workout

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Workout extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'repetitions',
+        'weight',
+        'break_time',
+        'day',
+        'observations',
+        'time',
+        'user_id',   
+        'student_id',   
+        'exercise_id' 
+                
+    ];
+    
+    protected $hidden = ['created_at','updated_at'];
 }
