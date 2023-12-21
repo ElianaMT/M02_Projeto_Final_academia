a81098c6c63b18f7dff30ff1d6ca96565d4e68cc
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-20 22:06:14 -0500

    feat:traze id do estudante

--- app/Http/Controllers/TreinoReportController.php
+++ app/Http/Controllers/TreinoReportController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class TreinoReportController extends Controller
 {
-    //
+    public function showTreino(Request $request){
+        $id = $request->input('id');
+
+        $student = Student::find($id);
+
+        return $student;
+    }
 }
