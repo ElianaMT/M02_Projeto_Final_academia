2d73c73934b2196097d5a1d65abe56213b79678f
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-21 12:36:20 -0500

    refactor:adiciona informacao para cada dia da semana
    Prepara variavels para pdf

--- app/Http/Controllers/TreinoReportController.php
+++ app/Http/Controllers/TreinoReportController.php
@@ -17,8 +17,105 @@ class TreinoReportController extends Controller
         ->find($id);
 
         $name = $student->name;
-        $workouts = $student->workouts;
-        $workoutDomingo = $workouts->filter(function ($workout) {
+        $workoutsDay = $student->workouts;
+
+        $workoutSegunda = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'SEGUNDA';
+        });
+        
+        // Iterar sobre os workouts da segunda
+        foreach ($workoutSegunda as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutTerca = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'TERCA';
+        });
+        
+        // Iterar sobre os workouts da terca
+        foreach ($workoutTerca as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutQuarta = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'QUARTA';
+        });
+        
+        // Iterar sobre os workouts da quarta
+        foreach ($workoutQuarta as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutQuinta = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'QUINTA';
+        });
+        
+        // Iterar sobre os workouts da quinta
+        foreach ($workoutQuinta as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutSexta = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'SEXTA';
+        });
+        
+        // Iterar sobre os workouts da Sexta
+        foreach ($workoutSexta  as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutSabado = $workoutsDay->filter(function ($workout) {
+            return strtoupper($workout->day) === 'SÁBADO';
+        });
+        
+        // Iterar sobre os workouts do dia sabado
+        foreach ($workoutSabado as $workout) {
+            $workout_id = $workout->id;
+            $exercise_id = $workout->exercise_id;
+            $repetitions = $workout->repetitions;
+            $weight = $workout->weight;
+            $break_time = $workout->break_time;
+            $day = $workout->day;
+            $observations = $workout->observations;
+            $time = $workout->time;
+        }
+
+        $workoutDomingo = $workoutsDay->filter(function ($workout) {
             return strtoupper($workout->day) === 'DOMINGO';
         });
         
@@ -37,13 +134,11 @@ class TreinoReportController extends Controller
 
         $pdf = Pdf::loadView('pdfs.treinoStudent',[
             'name' => $name,
-            'workouts' => $workouts,
-            'DOMINGO'=>$workoutDomingo,
-            
+            'workouts' => $workoutsDay,            
         ]);
 
-        return $student;
+        //return $student;
 
-       //return $pdf->stream(('treino.pdf'));
+       return $pdf->stream(('treino.pdf'));
     }
 }
