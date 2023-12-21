8aac2b25acda9582395c2a10b3ea9bf47e2000da
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-21 12:37:34 -0500

    feat:gera template para treino em formato de lista
    Por cada estudante

--- resources/views/pdfs/treinoStudent.blade.php
+++ resources/views/pdfs/treinoStudent.blade.php
@@ -2,21 +2,61 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=prueba, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treino PDF</title>
+
+    <!-- Add custom styles -->
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            color: #333;
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .header {
+            text-align: left;
+        }
+
+        h1 {
+            color: #FFA500; /* Orange */
+        }
+
+        h2 {
+            color: #FFA500; /* Orange */
+        }
+
+        div {
+            border: 1px solid #FFA500; /* Orange border */
+            padding: 15px;
+            margin-bottom: 20px;
+            border-radius: 10px;
+        }
+
+        p {
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
-    <head>{{$name}}</head>
-    <br>
-    <p> {{$workouts}}</p>
-    <br>  
-    <p> {{$DOMINGO}}</p>
+    <div class="header">
+        <h1>Plan de Treinos - {{ $name }}</h1>
+        <p class="company-name">TRAINSYS COMPANY</p>
+    </div>
 
-
-
-    
-    
-    
+    @foreach($workouts as $workout)
+        <div>
+            <h2>{{ $workout->day }}</h2>
+            <ul>
+                <li><strong>Exercise:</strong> {{ $workout->exercise_id }}</li>
+                <li><strong>Repetitions:</strong> {{ $workout->repetitions }}</li>
+                <li><strong>Weight:</strong> {{ $workout->weight }}</li>
+                <li><strong>Break Time:</strong> {{ $workout->break_time }}</li>
+                <li><strong>Observations:</strong> {{ $workout->observations }}</li>
+                <li><strong>Time:</strong> {{ $workout->time }}</li>
+            </ul>
+        </div>
+    @endforeach
 </body>
 </html>
