9e1acd181480accf9ca875d7f026c9164c0bb561
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-21 00:57:46 -0500

    feat: exporto en pdf os workouts

--- app/Http/Controllers/TreinoReportController.php
+++ app/Http/Controllers/TreinoReportController.php
@@ -11,13 +11,22 @@ class TreinoReportController extends Controller
     public function showTreino(Request $request){
         $id = $request->input('id');
 
-        $student = Student::find($id);
+        $student = Student::
+        with('workouts')
+        ->with('exercises')
+        ->find($id);
 
         $name = $student->name;
+        $workouts = $student->workouts;
+        //$exercises = $student->workouts->exercises;
 
         $pdf = Pdf::loadView('pdfs.treinoStudent',[
-            'name' => $name
+            'name' => $name,
+            'workouts' => $workouts,
+           // 'description' => $name
         ]);
+
+        //return $student;
 
         return $pdf->stream(('treino.pdf'));
     }
--- resources/views/pdfs/treinoStudent.blade.php
+++ resources/views/pdfs/treinoStudent.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     {{$name}}
+    {{$workouts}}
 </body>
 </html>
