55d6c44681222f9a8b71ae6560002df3a736a0ae
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-21 20:23:50 -0500

    feat:cria metodo index-dashboard
    Count para registered_students e  registered_exercises

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Mail\SendWelcomeUser;
+use App\Models\Exercise;
 use App\Models\User;
 use App\Models\Plan;
+use App\Models\Student;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -37,7 +41,28 @@ class UserController extends Controller
          return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
      }
- 
+
+     public function index(Request $request)
+    {
+        try {
+            $user = Auth::user();
+
+            $registered_students = Student::where('user_id', $request->user()->id)->count();
+            $registered_exercises = Exercise::where('user_id', $request->user()->id)->count();
+
+            $finalResponse =[
+                'registered_students' => $registered_students,
+                'registered_exercises' => $registered_exercises,
+            ];
+
+            return $finalResponse;
+        } catch (Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+
+    
  }
    
 
