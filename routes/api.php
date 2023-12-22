dd98582bd0e6945951351057f325b05c4585959d
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-22 00:34:17 -0500

    refactor: muda nome da rota middleware

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TreinoReportController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkoutController;
+use App\Http\Middleware\ValidateLimitStudentsToUser;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -16,7 +17,7 @@ Route::delete('exercises/{id}', [ExerciseController::class, 'destroy']);
 
 Route::get('students/treino', [TreinoReportController::class, 'showTreino']);
 Route::get('students/{id}', [StudentController::class, 'show']);
-Route::post('students', [StudentController::class, 'store'])->middleware(['validate-limit-account']);
+Route::post('students', [StudentController::class, 'store'])->middleware([ValidateLimitStudentsToUser::class]);
 Route::get('students', [StudentController::class, 'index']);
 Route::delete('students/{id}', [StudentController::class, 'destroy']);
 Route:: put('students/{id}', [StudentController::class,'update']);
