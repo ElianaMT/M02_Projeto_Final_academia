0fe68bbffe77f9be3ec391c5554e00c4f25d0098
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-23 13:12:58 -0500

    refactor: muda codigo de metodo store-Listagem de treinos do estudante
    Adiciona codigo que obten uma resposta com os exercicios cadastrados para cada treino

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -7,7 +7,6 @@ use App\Models\Workout;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Rule;
 use Symfony\Component\HttpFoundation\Response;
 
 class WorkoutController extends Controller
@@ -24,20 +23,24 @@ class WorkoutController extends Controller
             //inicializa uma query
             $workouts = Workout::query();
 
-            //verifica o filtro
+            // Verifica o filtro do student_id
             if ($request->has('student_id') && !empty($filters['student_id'])) { // mostra todos os estudantes por id asi no body eu nao coloque nada
                 $workouts->where('student_id', $filters['student_id']);  //filtra a tabela workouts por student_id        
             }
 
-            //retorna resultado
-            $columnOrder = $request->has('order') && !empty($filters['order']) ? $filters['order'] : 'student_id';
-            $workouts = $workouts->orderBy($columnOrder)->get();
+            // Obter os resultados ordenados por día da semana
+            $workouts = $workouts->orderBy('day')->get();
 
+            // Verifica sim se encontraron workouts para el student_id 
+            if ($request->has('student_id') && !empty($filters['student_id']) && $workouts->isEmpty()) {
+                return $this->error('Id. não existe!.Não existen treinos para o student_id proporcionado.', Response::HTTP_BAD_REQUEST);
+            }
 
+            // Inicia o array de resultados
             $results = [];
 
             foreach ($workouts as $workout) {
-                $day = ($workout->day);
+                $day = strtoupper($workout->day); // Asegura que o dia esté em mayúsculas
                 $exercise_id = $workout->exercise_id;
                 $exercise_description = $workout->exercise->description;
                 $repetitions = $workout->repetitions;
@@ -46,34 +49,35 @@ class WorkoutController extends Controller
                 $observations = $workout->observations;
                 $time = $workout->time;
 
-               
-                // Adiciona as propiedades ao dia correspondente
-                $results[$day]['exercise_id'] = $exercise_id;
-                $results[$day]['exercise_description'] = $exercise_description;
-                $results[$day]['repetitions'] = $repetitions;                
-                $results[$day]['weight'] = $weight;
-                $results[$day]['break_time'] = $break_time;
-                $results[$day]['observations'] = $observations;
-                $results[$day]['time'] = $time;
+                // Response, workout por dia
+                $results[$day][] = [
+                    'exercise_id' => $exercise_id,
+                    'exercise_description' => $exercise_description,
+                    'repetitions' => $repetitions,
+                    'weight' => $weight,
+                    'break_time' => $break_time,
+                    'observations' => $observations,
+                    'time' => $time,
+                ];
             }
 
             // Ordena os resultados por dias de la semana
-        $diasOrdenados = ['SEGUNDA', 'TERCA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'DOMINGO'];
-        $resultadosOrdenados = [];
+            $orderedDays = ['SEGUNDA', 'TERCA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'DOMINGO'];
+            $finalResults = [];
 
-        foreach ($diasOrdenados as $dia) {
-            // Verifica se há dados para o dia atual
-            if (isset($results[$dia])) {
-                // Adiciona os dados ao novo array $resultadosOrdenados
-                $resultadosOrdenados[$dia] = $results[$dia];
+            foreach ($orderedDays as $day) {
+                // Verifica sim hay dados para o día actual
+                if (isset($results[$day])) {
+                    // Agrega os dados ao novo array $finalResults
+                    $finalResults[$day] = $results[$day];
+                }
             }
-        }
 
             // Array final
             $finalResponse = [
-                'student_id' => $workouts[0]->student->id,
-                'student_name' => $workouts[0]->student->name,
-                'workouts' => $resultadosOrdenados
+                'student_id' => isset($workouts[0]) ? $workouts[0]->student->id : null,
+                'student_name' => isset($workouts[0]) ? $workouts[0]->student->name : null,
+                'workouts' => $finalResults,
             ];
 
             return $finalResponse;
@@ -82,59 +86,58 @@ class WorkoutController extends Controller
         }
     }
 
+
     public function store(Request $request)
-{
-    try {
-        // Obtengo usuario autenticado
-        $user = Auth::user();
-        $data = $request->all();
+    {
+        try {
+            // Obtengo usuario autenticado
+            $user = Auth::user();
+            $data = $request->all();
 
-        $request->validate([
-            'student_id' => 'required|exists:students,id,user_id,' . $user->id,
-            'exercise_id' => 'required|exists:exercises,id',
-            'repetitions' => 'required|int',
-            'weight' => 'required|decimal:2', // Hasta 2 decimales
-            'break_time' => 'required|int',
-            'day' => 'required|in:SEGUNDA,TERCA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
-            'observations' => 'nullable|string',
-            'time' => 'required|string|max:10',
-        ]);
+            $request->validate([
+                'student_id' => 'required|exists:students,id,user_id,' . $user->id,
+                'exercise_id' => 'required|exists:exercises,id',
+                'repetitions' => 'required|int',
+                'weight' => 'required|decimal:2', // Hasta 2 decimales
+                'break_time' => 'required|int',
+                'day' => 'required|in:SEGUNDA,TERCA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
+                'observations' => 'nullable|string',
+                'time' => 'required|string|max:10',
+            ]);
 
-        // Valida que o exercicio nao se repita para o mismo dia
-        $existingWorkout = Workout::where([
-            'student_id' => $request->input('student_id'),
-            'exercise_id' => $request->input('exercise_id'),
-            'day' => $request->input('day'),
-        ])->first();
+            // Valida que o exercicio nao se repita para o mismo dia
+            $existingWorkout = Workout::where([
+                'student_id' => $request->input('student_id'),
+                'exercise_id' => $request->input('exercise_id'),
+                'day' => $request->input('day'),
+            ])->first();
 
-        if ($existingWorkout) {
-            return $this->error('Este exercício já foi cadastrado para este dia', Response::HTTP_CONFLICT);
+            if ($existingWorkout) {
+                return $this->error('Este exercício já foi cadastrado para este dia', Response::HTTP_CONFLICT);
+            }
+
+            $workout = Workout::create([...$request->all(), 'user_id' => $user->id]);
+
+            // Encontra o estudante relacionado com o treino
+            $student = Student::find($workout->student_id);
+
+            // Verifica si o estudante existe
+            if (!$student) {
+                return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
+            }
+
+            // Verifica si el usuario autenticado es igual al user_id del estudiante
+            if ($user->id !== $student->user_id) {
+                return $this->error('Não tem permissão para visualizar este treino', Response::HTTP_FORBIDDEN);
+            }
+
+            $responseData = [
+                'workout' => $workout
+            ];
+
+            return $responseData;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
-
-        $workout = Workout::create([...$request->all(), 'user_id' => $user->id]);
-
-        // Encontra o estudante relacionado com o treino
-        $student = Student::find($workout->student_id);
-
-         // Verifica si o estudante existe
-        if (!$student) {
-            return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
-        }
-
-        // Verifica si el usuario autenticado es igual al user_id del estudiante
-        if ($user->id !== $student->user_id) {
-            return $this->error('Não tem permissão para visualizar este treino', Response::HTTP_FORBIDDEN);
-        }
-
-        $responseData = [
-            'workout' => $workout
-        ];
-
-        return $responseData;
-    } catch (\Exception $exception) {
-        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
     }
-}
-
-
 }
