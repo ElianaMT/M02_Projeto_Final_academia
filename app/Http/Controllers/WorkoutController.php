3bd84ff290e806caaee65e585184fc9f6076b26b
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-23 01:16:24 -0500

    refactor: muda codigo do cadastro de treino-workout
    Considera que o mismo exercício nao se repita en el mismo dia

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -83,56 +83,58 @@ class WorkoutController extends Controller
     }
 
     public function store(Request $request)
-    {
-        try {
-            // Obtenho usuario autenticado
-            $user = Auth::user();
-            $data = $request->all();
+{
+    try {
+        // Obtengo usuario autenticado
+        $user = Auth::user();
+        $data = $request->all();
 
-            $request->validate([
-                'student_id' => 'required|exists:students,id,user_id,' . $user->id,
-                'exercise_id' => 'required|exists:exercises,id',
-                'repetitions' => 'required|int',
-                'weight' => 'required|decimal:2', // Hasta 2 decimales
-                'break_time' => 'required|int',
-                'day' => [
-                    'required',
-                    'string',
-                    Rule::in(['SEGUNDA', 'TERCA', 'QUARTA', 'QUINTA', 'SEXTA', 'SÁBADO', 'DOMINGO']),
-                    Rule::unique('workouts')->where(function ($query) use ($user, $data) {
-                        return $query->where('user_id', $user->id)
-                            ->where('student_id', $data['student_id']);
-                    }),
-                ],
+        $request->validate([
+            'student_id' => 'required|exists:students,id,user_id,' . $user->id,
+            'exercise_id' => 'required|exists:exercises,id',
+            'repetitions' => 'required|int',
+            'weight' => 'required|decimal:2', // Hasta 2 decimales
+            'break_time' => 'required|int',
+            'day' => 'required|in:SEGUNDA,TERCA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
+            'observations' => 'nullable|string',
+            'time' => 'required|string|max:10',
+        ]);
 
-                'observations' => 'nullable|string',
-                'time' => 'required|string|max:10',
-            ]);
+        // Valida que o exercicio nao se repita para o mismo dia
+        $existingWorkout = Workout::where([
+            'student_id' => $request->input('student_id'),
+            'exercise_id' => $request->input('exercise_id'),
+            'day' => $request->input('day'),
+        ])->first();
 
-            $workout = Workout::create([...$request->all(), 'user_id' => $user->id]);
-
-
-            // Encontra o estudante relacionado com o treino
-            $student = Student::find($workout->student_id);
-
-            // Verifica si o estudante existe
-            if (!$student) {
-                return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
-            }
-
-            // Verifica si usuario autenticado es igual al user_id del estudiante
-            if ($user->id !== $student->user_id) {
-                return $this->error('Não tem permissão para visualizar este treino', Response::HTTP_FORBIDDEN);
-            }
-
-
-            $responseData = [
-                'workout' => $workout
-            ];
-
-            return $responseData;
-        } catch (\Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        if ($existingWorkout) {
+            return $this->error('Este exercício já foi cadastrado para este dia', Response::HTTP_CONFLICT);
         }
+
+        $workout = Workout::create([...$request->all(), 'user_id' => $user->id]);
+
+        // Encontra o estudante relacionado com o treino
+        $student = Student::find($workout->student_id);
+
+         // Verifica si o estudante existe
+        if (!$student) {
+            return $this->error('Estudante não encontrado', Response::HTTP_NOT_FOUND);
+        }
+
+        // Verifica si el usuario autenticado es igual al user_id del estudiante
+        if ($user->id !== $student->user_id) {
+            return $this->error('Não tem permissão para visualizar este treino', Response::HTTP_FORBIDDEN);
+        }
+
+        $responseData = [
+            'workout' => $workout
+        ];
+
+        return $responseData;
+    } catch (\Exception $exception) {
+        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
     }
+}
+
+
 }
